e124dbd2d2538384d80c9f67c12daa67eb83a29b
Hyejin724 <imhaejin98@gmail.com>
2023-11-03 15:22:35 +0900

    Drag complete

--- src/components/List.js
+++ src/components/List.js
@@ -21,10 +21,32 @@ export default function List({todoData, setTodoData}) {
         setTodoData(newTodoData)
       }
 
+      const handleEnd = (result) => {
+        //result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함됩니다.
+        console.log('result', result);
+
+        //목적지가 없으면(이벤트 취소) 이 함수를 종료합니다.
+        if(!result.destination) return;
+
+        //리액트 불변성을 지켜주기 위해 새로운 todoData 생성
+        const newTodoData = todoData;
+
+        // 1. 변경시키는 아이템을 배열에서 지워줍니다.
+        // 2. return 값으로 지워진 아이템을 잡아줍니다.
+        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+        
+        // 원하는 자리에 redorerdItem을 insert 해줍니다.
+        newTodoData.splice(result.destination.index, 0, reorderedItem);
+        setTodoData(newTodoData);
+      }
+
+  
   return (
     <div>
-      <DragDropContext>
-        <Droppable droppableId='HI'>
+      <DragDropContext onDragEnd={handleEnd}>
+
+        <Droppable droppableId="to-doss">
+
           {(provided)=>(
             <div {...provided.droppableProps} ref={provided.innerRef}>
             
@@ -35,7 +57,7 @@ export default function List({todoData, setTodoData}) {
             {(provided, snapshot) => (
 
             <div key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}
-            className ={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100" } "flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}>
+            className ={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100" } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}>
            
             <div className="items-center ">
             <input type="checkbox" 
